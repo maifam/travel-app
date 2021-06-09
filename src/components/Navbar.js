2fe43d1cf7df83f96ded9acd2e4baddcb5f70cc5
Mai Pham <maitphm@gmail.com>
2021-06-08 23:58:59 -0400

    installed react-router

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import React, {useState} from 'React';
+
+function Navbar() {
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+
+                </div>
+            </nav>
+        
+        </>
+    )
+}
+
+export default Navbar
