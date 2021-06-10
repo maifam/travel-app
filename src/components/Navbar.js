e507bdf78e9c81399bef3c8e79f51bc297bd5bca
Mai Pham <maitphm@gmail.com>
2021-06-09 22:53:16 -0400

    installed icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
-import React from 'react';
+
 import React, {useState} from 'React';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-
+                    <Link to='/' className='navbar-logo'>
+                        TRAVEL <i className='fab fa-typo3'/>
+                    </Link>
                 </div>
             </nav>
         
