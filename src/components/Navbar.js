a791d22aa197990353f39d892f4824de040a5160
Mai Pham <maitphm@gmail.com>
2021-06-09 23:10:58 -0400

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 
-import React, {useState} from 'React';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click);
+    const closedMobileMenu = () => setClick(false)
+
     return (
         <>
             <nav className='navbar'>
@@ -10,7 +15,33 @@ function Navbar() {
                     <Link to='/' className='navbar-logo'>
                         TRAVEL <i className='fab fa-typo3'/>
                     </Link>
+                    <div className='menu-icon' onCLick={handleClick}>
+                        <i className={click ? 'fas-fa-times' : 'fas-fa-bars'}/>
+                    </div>
+                    <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closedMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closedMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick={closedMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closedMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
+
             </nav>
         
         </>
