95c60e2aed0eaad9a38cb5ac014fab8a81bcb56c
Mai Pham <maitphm@gmail.com>
2021-06-17 00:40:05 -0400

    cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -16,7 +16,38 @@ function Cards() {
                     label='Adventure'
                     path='/services'
                     />
+                    <CardItem
+                    src='images/img-2.jpg'
+                    text='Travel through the islands of Bali 
+                    in a private cruise'
+                    label='Luxury'
+                    path='/services'
+                    />
                 </ul>
+                <ul className='cards__items'>
+                    <CardItem
+                    src='images/img-3.jpg'
+                    text='Explore the hidden waterfall deep 
+                    inside the Amazon jungle'
+                    label='Adventure'
+                    path='/services'
+                    />
+                    <CardItem
+                    src='images/img-4.jpg'
+                    text='Travel through the islands of Bali 
+                    in a private cruise'
+                    label='Luxury'
+                    path='/services'
+                    />
+                    <CardItem
+                    src='images/img-8.jpg'
+                    text='Travel through the islands of Bali 
+                    in a private cruise'
+                    label='Luxury'
+                    path='/services'
+                    />
+                </ul>
+
                 </div>
             </div>
         </div>
